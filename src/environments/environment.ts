79095dd92e49ec1715a9362f330e59ba598aaa4b
Vishal Rajole <vishalrajole@Vishals-MBP.fritz.box>
2021-06-30 18:16:27 +0200

    setting up env variables for build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  BASE_URL: 'https://api.themoviedb.org/3/',
-  API_KEY: '6dbcf6b4d31b238d4f21cd54be3ce1c1',
+  BASE_URL: '',
+  API_KEY: '',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
